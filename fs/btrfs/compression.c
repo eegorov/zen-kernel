f5d00c52fe493ff6a38a85d25c7f4d6e97146d24
David Sterba <dsterba@suse.cz>
2012-01-25 00:08:06 +0100

    btrfs: prepare incompat flags for more compression methods
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: lz4: add wrapper functions and enable it
    
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: add lz4hc incompat bits
    
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: add lz4hc wrapper and enable it
    
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: reduce duplicate code in lz4_wrapper.c
    
    Add generic functions for compress_pages and alloc that accept an argument to
    enable high compression, and replace the lz4/lz4hc variants of both with
    wrappers for these.
    
    Signed-off-by: Andrew Mahone <andrew.mahone@gmail.com>
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    Btrfs need LZ4 api
    
    Hot fix previous merge

--- fs/btrfs/compression.c
+++ fs/btrfs/compression.c
@@ -808,6 +808,8 @@ static const struct btrfs_compress_op * const btrfs_compress_op[] = {
 	&btrfs_zlib_compress,
 	&btrfs_lzo_compress,
 	&btrfs_zstd_compress,
+	&btrfs_lz4_compress,
+	&btrfs_lz4hc_compress,
 };
 
 void __init btrfs_init_compress(void)
--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -538,6 +538,21 @@ int btrfs_parse_options(struct btrfs_fs_info *info, char *options,
 				btrfs_clear_opt(info->mount_opt, NODATACOW);
 				btrfs_clear_opt(info->mount_opt, NODATASUM);
 				btrfs_set_fs_incompat(info, COMPRESS_ZSTD);
+			} else if (strcmp(args[0].from, "lz4") == 0) {
+				compress_type = "lz4";
+				info->compress_type = BTRFS_COMPRESS_LZ4;
+				btrfs_set_opt(info->mount_opt, COMPRESS);
+				btrfs_clear_opt(info->mount_opt, NODATACOW);
+				btrfs_clear_opt(info->mount_opt, NODATASUM);
+				btrfs_set_fs_incompat(info, COMPRESS_LZ4);
+				no_compress = 0;
+			} else if (strcmp(args[0].from, "lz4hc") == 0) {
+				compress_type = "lz4hc";
+				info->compress_type = BTRFS_COMPRESS_LZ4HC;
+				btrfs_set_opt(info->mount_opt, COMPRESS);
+				btrfs_clear_opt(info->mount_opt, NODATACOW);
+				btrfs_clear_opt(info->mount_opt, NODATASUM);
+				btrfs_set_fs_incompat(info, COMPRESS_LZ4);
 				no_compress = 0;
 			} else if (strncmp(args[0].from, "no", 2) == 0) {
 				compress_type = "no";
@@ -1263,6 +1278,10 @@ static int btrfs_show_options(struct seq_file *seq, struct dentry *dentry)
 			compress_type = "zlib";
 		else if (info->compress_type == BTRFS_COMPRESS_LZO)
 			compress_type = "lzo";
+                else if (info->compress_type == BTRFS_COMPRESS_LZ4)
+                        compress_type = "lz4";
+		else if (info->compress_type == BTRFS_COMPRESS_LZ4HC)
+			compress_type = "lz4hc";
 		else
 			compress_type = "zstd";
 		if (btrfs_test_opt(info, FORCE_COMPRESS))
