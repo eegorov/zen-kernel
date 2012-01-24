aebbfae38b6a83f4489d80c717e4d765eb5eaa7b
David Sterba <dsterba@suse.cz>
2012-01-25 00:08:06 +0100

    btrfs: prepare incompat flags for more compression methods
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: lz4: add wrapper functions and enable it
    
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: add lz4hc incompat bits
    
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: add lz4hc wrapper and enable it
    
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    btrfs: reduce duplicate code in lz4_wrapper.c
    
    Add generic functions for compress_pages and alloc that accept an argument to
    enable high compression, and replace the lz4/lz4hc variants of both with
    wrappers for these.
    
    Signed-off-by: Andrew Mahone <andrew.mahone@gmail.com>
    Signed-off-by: David Sterba <dsterba@suse.cz>
    
    Btrfs need LZ4 api
    
    Hot fix previous merge

--- fs/btrfs/compression.h
+++ fs/btrfs/compression.h
@@ -105,7 +105,10 @@ enum btrfs_compression_type {
 	BTRFS_COMPRESS_ZLIB  = 1,
 	BTRFS_COMPRESS_LZO   = 2,
 	BTRFS_COMPRESS_ZSTD  = 3,
-	BTRFS_NR_COMPRESS_TYPES = 4,
+	BTRFS_COMPRESS_LZ4   = 4,
+	BTRFS_COMPRESS_LZ4HC = 5,
+	BTRFS_COMPRESS_TYPES = 6,
+	BTRFS_NR_COMPRESS_TYPES = 7,
 };
 
 struct workspace_manager {
@@ -135,6 +138,8 @@ struct btrfs_compress_op {
 extern const struct btrfs_compress_op btrfs_heuristic_compress;
 extern const struct btrfs_compress_op btrfs_zlib_compress;
 extern const struct btrfs_compress_op btrfs_lzo_compress;
+extern const struct btrfs_compress_op btrfs_lz4_compress;
+extern const struct btrfs_compress_op btrfs_lz4hc_compress;
 extern const struct btrfs_compress_op btrfs_zstd_compress;
 
 const char* btrfs_compress_type2str(enum btrfs_compression_type type);
