5e579dc9a974d9481cdd4307d7a5a30d30ee8d26
eegorov <egor.y.egorov@gmail.com>
2014-05-23 13:07:56 +0700

    HP 250GB SATA disk VB0250EAVER seem to have FPMDA_AA issues for 6.12

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -4039,6 +4039,9 @@ static const struct ata_dev_quirks_entry __ata_dev_quirks[] = {
 						ATA_QUIRK_NOLPM },
 	{ "VB0250EAVER",	"HPG7",		ATA_QUIRK_BROKEN_FPDMA_AA },
 
+	/* HP 250GB SATA disk VB0250EAVER seem to have FPMDA_AA issues */
+	{ "VB0250EAVER",	"HPG0",		ATA_QUIRK_BROKEN_FPDMA_AA },
+
 	/* Blacklist entries taken from Silicon Image 3124/3132
 	   Windows driver .inf file - also several Linux problem reports */
 	{ "HTS541060G9SA00",    "MB3OC60D",     ATA_QUIRK_NONCQ },
