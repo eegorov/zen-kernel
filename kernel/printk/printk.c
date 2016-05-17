4acb8dd1adc8c471561fb2c9fc57b94df136118a
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2016-05-17 23:02:58 +0200

    CKO (Colored Kernel Output) patch
    Patch by eegorov. Adjusted for v5.9.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1819,7 +1819,8 @@ static int console_trylock_spinning(void)
  * log_buf[start] to log_buf[end - 1].
  * The console_lock must be held.
  */
-static void call_console_drivers(const char *ext_text, size_t ext_len,
+static void call_console_drivers(int level,
+				 const char *ext_text, size_t ext_len,
 				 const char *text, size_t len)
 {
 	static char dropped_text[64];
@@ -1849,11 +1850,11 @@ static void call_console_drivers(const char *ext_text, size_t ext_len,
 		    !(con->flags & CON_ANYTIME))
 			continue;
 		if (con->flags & CON_EXTENDED)
-			con->write(con, ext_text, ext_len);
+			con->write(con, ext_text, ext_len, level);
 		else {
 			if (dropped_len)
-				con->write(con, dropped_text, dropped_len);
-			con->write(con, text, len);
+				con->write(con, dropped_text, dropped_len, level);
+			con->write(con, text, len, level);
 		}
 	}
 }
@@ -2173,7 +2174,8 @@ static ssize_t msg_print_ext_body(char *buf, size_t size,
 				  struct dev_printk_info *dev_info) { return 0; }
 static void console_lock_spinning_enable(void) { }
 static int console_lock_spinning_disable_and_check(void) { return 0; }
-static void call_console_drivers(const char *ext_text, size_t ext_len,
+static void call_console_drivers(int level,
+				 const char *ext_text, size_t ext_len,
 				 const char *text, size_t len) {}
 static bool suppress_message_printing(int level) { return false; }
 
@@ -2195,7 +2197,7 @@ asmlinkage __visible void early_printk(const char *fmt, ...)
 	n = vscnprintf(buf, sizeof(buf), fmt, ap);
 	va_end(ap);
 
-	early_console->write(early_console, buf, n);
+	early_console->write(early_console, buf, n, 0);
 }
 #endif
 
@@ -2510,6 +2512,7 @@ again:
 	for (;;) {
 		size_t ext_len = 0;
 		size_t len;
+		int level;
 
 		printk_safe_enter_irqsave(flags);
 		raw_spin_lock(&logbuf_lock);
@@ -2522,7 +2525,8 @@ skip:
 			console_seq = r.info->seq;
 		}
 
-		if (suppress_message_printing(r.info->level)) {
+		level = r.info->level;
+		if (suppress_message_printing(level)) {
 			/*
 			 * Skip record we have buffered and already printed
 			 * directly to the console when we received it, and
@@ -2567,7 +2571,7 @@ skip:
 		console_lock_spinning_enable();
 
 		stop_critical_timings();	/* don't trace print latency */
-		call_console_drivers(ext_text, ext_len, text, len);
+		call_console_drivers(level, ext_text, ext_len, text, len);
 		start_critical_timings();
 
 		if (console_lock_spinning_disable_and_check()) {
