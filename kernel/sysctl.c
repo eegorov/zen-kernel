831c5de19ed8ae88bcd45cd459d3a8adbf5efc86
Egor Y. Egorov <egor.y.egorov@gmail.com>
2018-11-14 10:07:23 +0600

    4400_alpha-sysctl-uac.patch

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -160,6 +160,11 @@ extern char fbcon_decor_path[];
 #ifdef CONFIG_INOTIFY_USER
 #include <linux/inotify.h>
 #endif
+
+#ifdef CONFIG_ALPHA_UAC_SYSCTL
+extern struct ctl_table uac_table[];
+#endif
+
 #ifdef CONFIG_SPARC
 #endif
 
@@ -1907,6 +1912,13 @@ static struct ctl_table debug_table[] = {
 		.extra2		= &one,
 	},
 #endif
+#ifdef CONFIG_ALPHA_UAC_SYSCTL
+	{
+	        .procname   = "uac",
+		.mode       = 0555,
+	        .child      = uac_table,
+	 },
+#endif /* CONFIG_ALPHA_UAC_SYSCTL */
 	{ }
 };
 
