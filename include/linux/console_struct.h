a9981b956a5b64e1a93e82d65e79530dc8e2f3a6
Egor Y. Egorov <egor.y.egorov@gmail.com>
2018-11-14 10:07:23 +0600

    4200_fbcondecor.patch

--- include/linux/console_struct.h
+++ include/linux/console_struct.h
@@ -21,6 +21,7 @@ struct uni_pagedir;
 struct uni_screen;
 
 #define NPAR 16
+#include <linux/console_decor.h>
 
 /*
  * Example: vc_data of a console that was scrolled 3 lines down.
@@ -142,6 +143,8 @@ struct vc_data {
 	struct uni_pagedir **vc_uni_pagedir_loc; /* [!] Location of uni_pagedir variable for this console */
 	struct uni_screen *vc_uni_screen;	/* unicode screen content */
 	bool vc_panic_force_write; /* when oops/panic this VC can accept forced output/blanking */
+
+	struct vc_decor vc_decor;
 	/* additional information is in vt_kern.h */
 };
 
